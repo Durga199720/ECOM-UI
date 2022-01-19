4ec8b9d4be6f3f8f461f917c0d257242ce8847b4
Durga199720 <60807220+Durga199720@users.noreply.github.com>
2022-01-19 14:55:53 +0530

    Add files via upload

--- src/app/service/api.service.ts
+++ src/app/service/api.service.ts
@@ -1,9 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  //getProducts() {
+    // return this.http.get<any>("https://fakestoreapi.com/products/")
+    //   .pipe(map((res:any)=>{
+    // return res;
+    //   }))
+  //} another way
+  public getProduct(): Observable<any> {
+    return this.http.get("https://fakestoreapi.com/products/");
+  }
 }
+
